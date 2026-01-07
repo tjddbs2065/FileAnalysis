5d232811ac9763b1740bd17a0a07d98f70e99165
SeongYun <tjddbs2065@gmail.com>
2026-01-07 21:47:39 +0900

    feat: parse boot sector to fat32bootsector object

---
+++ FileAnalysis/Domain/Fat32/Fat32BootSector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAnalysis.Domain.Fat32
+{
+    internal class Fat32BootSector
+    {
+        private string OemName { get; }
+        private ushort BytesPerSector { get; }
+        private byte SectorsPerCluster { get; }
+        private ushort ReservedSectorCount { get; }
+        private byte NumOfFATs { get; }
+        private uint SectorsPerFAT { get; }
+        private uint RootCluster { get; }
+        private ushort FsInfoSector { get; }
+        private string FileSystemType { get; }
+
+
+        public Fat32BootSector(string oemName, ushort bytesPerSector, byte sectorsPerCluster, ushort reservedSectorCount, byte numOfFATs, uint sectorsPerFAT, uint rootCluster, ushort fsInfoSector, string fileSystemType)
+        {
+            OemName = oemName;
+            BytesPerSector = bytesPerSector;
+            SectorsPerCluster = sectorsPerCluster;
+            ReservedSectorCount = reservedSectorCount;
+            NumOfFATs = numOfFATs;
+            SectorsPerFAT = sectorsPerFAT;
+            RootCluster = rootCluster;
+            FsInfoSector = fsInfoSector;
+            FileSystemType = fileSystemType;
+        }
+
+        public override string ToString()
+        {
+            return OemName + "\n" + BytesPerSector + "\n" + SectorsPerCluster + "\n" + ReservedSectorCount + "\n" + NumOfFATs + "\n" + SectorsPerFAT + "\n" + RootCluster + "\n" + FsInfoSector + "\n" + FileSystemType;
+        }
+    }
+}
---
+++ FileAnalysis/Domain/Fat32/Fat32Context.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAnalysis.Domain.Fat32
+{
+    internal class Fat32Context
+    {
+        private string oemName { get; }
+        private long bytesPerSector {  get; }
+        private long sectorsPerCluster { get; }
+        private long reservedSectorCount { get; }
+        private long numOfFat {  get; }
+        private long sectorsPerFat { get; }
+        private long rootCluster {  get; }
+        private long fsInfoSector { get; }
+        private string fsType { get; }
+
+        public Fat32Context(string oemName, long bytesPerSector, long sectorsPerCluster, long reservedSectorCount, long numOfFat, long sectorsPerFat, long rootCluster, long fsInfoSector, string fsType)
+        {
+            this.oemName = oemName;
+            this.bytesPerSector = bytesPerSector;
+            this.sectorsPerCluster = sectorsPerCluster;
+            this.reservedSectorCount = reservedSectorCount;
+            this.numOfFat = numOfFat;
+            this.sectorsPerFat = sectorsPerFat;
+            this.rootCluster = rootCluster;
+            this.fsInfoSector = fsInfoSector;
+            this.fsType = fsType;
+        }
+    }
+}
